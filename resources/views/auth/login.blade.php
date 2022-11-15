3c23c52af72b6e45e602a77df51d5238a8672c8c
tomsZvirbulis <toms.zvirbulis@valmierastehnikums.lv>
2022-11-15 15:15:53 +0200

    login created

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,50 @@
 @extends('layouts.main')
 
-@section('content')
-    <h1>Login</h1>
-@endsection
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+      <div class="row justify-content-center align-items-center h-100">
+        <div class="col-12 col-lg-9 col-xl-7">
+          <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+            <div class="card-body p-4 p-md-5">
+              <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
+              <form method='POST' action='/login'>
+                @csrf
+                <div class="row">
+                  <div class="col-md-12 mb-4 pb-2">
+  
+                    <div class="form-outline">
+                      <input name='email' type="email" id="emailAddress" class="form-control form-control-lg" required />
+                      <label class="form-label" for="emailAddress">Email</label>
+                    </div>
+  
+                  </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12 mb-4 pb-2">
+    
+                      <div class="form-outline">
+                        <input name='password' type="password" id="password" class="form-control form-control-lg" required />
+                        <label class="form-label" for="emailAddress">Password</label>
+                      </div>
+    
+                    </div>
+                </div>         
+  
+                <div class="mt-4 pt-2">
+                  <input class="btn btn-primary btn-lg" type="submit" value="Login" />
+                </div>
+  
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  @if ($errors->any())
+  @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+  @endforeach
+  @endif
