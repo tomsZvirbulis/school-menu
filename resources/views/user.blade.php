975c0202d4f9c620bab8f34f0786956330c3e0f7
tomsZvirbulis <toms.zvirbulis@valmierastehnikums.lv>
2022-11-17 14:00:17 +0200

    worker register progress

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,5 +1,58 @@
 @extends('layouts.main')
-
+<link rel="stylesheet" href="{{ asset('css/user.css') }}">
 @section('content')
-    <h1>user</h1>
+@if (Auth::user()->master == 1)
+    <div class="user">
+        <div class="reg-worker">
+            <form action="">
+                <h1>Register new worker</h1>
+                <!-- 2 column grid layout with text inputs for the first and last names -->
+                <div class="row mb-4">
+                    <div class="col">
+                        <div class="form-outline">
+                        <input type="text" id="form6Example1" class="form-control" />
+                        <label class="form-label" for="form6Example1">First name</label>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-outline">
+                        <input type="text" id="form6Example2" class="form-control" />
+                        <label class="form-label" for="form6Example2">Last name</label>
+                        </div>
+                    </div>
+                </div>
+            
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                <input type="email" id="form6Example5" class="form-control" />
+                <label class="form-label" for="form6Example5">Email</label>
+                </div>
+
+                <div class="row mb-4">
+                    <div class="col">
+                        <div class="form-outline">
+                        <input type="password" id="form6Example1" class="form-control" />
+                        <label class="form-label" for="form6Example1">Password</label>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-outline">
+                        <input type="password" id="form6Example2" class="form-control" />
+                        <label class="form-label" for="form6Example2">Confirm password</label>
+                        </div>
+                    </div>
+                </div>
+            
+                <!-- Submit button -->
+                <button onClick="event.preventDefault()" type="submit" class="btn btn-primary btn-block">Register worker</button>
+            </form>
+        </div>
+    </div>
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    @endif
+@endif
+
 @endsection
