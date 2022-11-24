67fa033deace2f9537e4e718a8001a331e48f2b6
tomsZvirbulis <toms.zvirbulis@valmierastehnikums.lv>
2022-11-24 13:07:41 +0200

    progress

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" integrity="sha384-xeJqLiuOvjUBq3iGOjvSQSIlwrpqjSHXpduPd6rQpuiM3f5/ijby8pCsnbu5S81n" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
--- resources/views/recepies.blade.php
+++ resources/views/recepies.blade.php
@@ -1,5 +1,103 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Recepies</h1>
+    <div class="recepies">
+        <button type="button" class="btn btn-success" onclick="addRecepie()">Add recepie</button>
+        <div calss='model'>
+            <form action='/test'>
+                <!-- 2 column grid layout with text inputs for the first and last names -->
+                <div class="row mb-4">
+                  <div class="col">
+                    <div class="form-outline">
+                      <input name='recepie-name' type="text" id="recepie-name" class="form-control" />
+                      <label class="form-label" for="recepie-name">Recepie name</label>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                    <div class="col">
+                      <div class="form-outline">
+                        <input name='prep-time' type="number" id="prep-time" class="form-control" />
+                        <label class="form-label" for="prep-time">Prep time</label>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="form-outline">
+                        <input name='cook-time' type="number" id="cook-time" class="form-control" />
+                        <label class="form-label" for="cook-time">Cook time</label>
+                      </div>
+                    </div>
+                </div>
+              
+                <div class="row mb-4">
+                    <div class="col">
+                      <div class="form-outline">
+                        <input name='calories' type="number" id="calories" class="form-control" />
+                        <label class="form-label" for="calories">Calories</label>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="form-outline">
+                        <input name='servings' type="number" id="servings" class="form-control" />
+                        <label class="form-label" for="servings">Servings</label>
+                      </div>
+                    </div>
+                </div>
+
+                <button type="button" class="btn btn-success" id='ingred-btn'>Add ingredient</button>
+                <div id='ingred-input'>
+
+                </div>
+                
+                <!-- Message input -->
+                <div class="form-outline mb-4">
+                  <textarea class="form-control" id="form6Example7" rows="4"></textarea>
+                  <label class="form-label" for="form6Example7">Additional information</label>
+                </div>
+              
+                <!-- Checkbox -->
+                <div class="form-check d-flex justify-content-center mb-4">
+                  <input class="form-check-input me-2" type="checkbox" value="" id="form6Example8" checked />
+                  <label class="form-check-label" for="form6Example8"> Create an account? </label>
+                </div>
+              
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-primary btn-block mb-4">Place order</button>
+              </form>
+        </div>
+        <table>
+            @foreach ($recepies as $recepie)
+                <tr>
+                    <td></td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+<script>
+    const addRecepie = () => {
+
+    }
+
+    $('#ingred-btn').click(function () {
+        event.preventDefault();
+        let str1 = `<div><h2 class="form-label">Ingredient #${$('#ingred-input div').length+1}</h2><label class="form-label" for="ingred-name">Ingredient name</label><input name="ingred-name" type="number" id="ingred-name" class="form-control" />`
+        let str = `<div><h2 class="form-label">Ingredient #${$('#ingred-input div').length+1}</h2><div class="row mb-4">
+            <div class="row mb-4">
+                    <div class="col">
+                      <div class="form-outline">
+                        <label class="form-label" for="ingred-name">Ingredient name</label>
+                        <input name='calories' type="number" id="ingred-name" class="form-control" />
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="form-outline">
+                        <label class="form-label" for="servings">Servings</label>
+                        <input name='servings' type="number" id="servings" class="form-control" />
+                      </div>
+                    </div>
+                </div></div>`
+        $('#ingred-input').append(str)
+    })
+</script>
 @endsection
